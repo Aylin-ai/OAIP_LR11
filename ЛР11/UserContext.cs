ea518d989fe4888ee6aa9a1bf8af1703b5da5dc5
Aylin-ai <69709234+Aylin-ai@users.noreply.github.com>
2022-04-28 18:28:36 +0300

    Add files via upload

---
+++ ЛР11/UserContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace ЛР11
+{
+    public class UserContext: DbContext
+    {
+        public UserContext() : base("DbConnection") { }
+        public DbSet<User> Users { get; set; }
+    }
+}
